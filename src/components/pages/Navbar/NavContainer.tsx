c67d8e605a22b188fdaa1fa48e479aafbb837e65
Anhpati <romain-felicianne@hotmail.fr>
2024-06-12 00:44:40 +0200

    ♻️ F04 : NAVBAR > Extract Navbar navigation container styles to NavbarContainer.

--- src/components/pages/Navbar/NavContainer.tsx
+++ src/components/pages/Navbar/NavContainer.tsx
@@ -1,9 +1,10 @@
+import styled from "styled-components";
 import { LogoContainer } from "../../ui/LogoContainer";
 import { LuArrowDown, LuArrowDownToLine, LuArrowUp, LuArrowUpToLine } from "react-icons/lu";
 
 export const NavContainer = () => {
     return (
-        <div className="nav-container">
+        <NavContainerStyled>
             <LogoContainer />
             <div className="buttons-container">
                 <button>
@@ -19,6 +20,40 @@ export const NavContainer = () => {
                     <LuArrowUpToLine />
                 </button>
             </div>
-        </div>
+        </NavContainerStyled>
     )
 }
+
+const NavContainerStyled = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .buttons-container {
+        flex: 1;
+        display: flex;
+        justify-content: space-around;
+
+        button {
+            padding: 10px;
+            background: #3e9b4f;
+            color: #FFF;
+            border: solid 1px #FFF;
+            border-radius: 5px;
+            font-size: 20px;
+            font-weight: 500;
+
+            &:hover {
+                background: #FFF;
+                color: #3e9b4f;
+                cursor: pointer;
+            }
+
+            &:active {
+                background: #3e9b4f;
+                color: #FFF;
+            }
+        }
+    }
+`;
