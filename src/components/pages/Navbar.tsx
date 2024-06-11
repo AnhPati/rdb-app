60787990379de64735439ad553c8a9dd7bc7c3b0
Anhpati <romain-felicianne@hotmail.fr>
2024-06-12 00:35:37 +0200

    💄 F04 : NAVBAR > Add nav buttons styles.

--- src/components/pages/Navbar.tsx
+++ src/components/pages/Navbar.tsx
@@ -49,5 +49,32 @@ const NavbarStyled = styled.nav`
     display: flex;
     justify-content: space-around;
     align-items: center;
+
+    .buttons-container {
+        flex: 1;
+        display: flex;
+        justify-content: space-around;
+
+        button {
+            padding: 10px;
+            background: #3e9b4f;
+            color: #FFF;
+            border: solid 1px #FFF;
+            border-radius: 5px;
+            font-size: 20px;
+            font-weight: 500;
+
+            &:hover {
+                background: #FFF;
+                color: #3e9b4f;
+                cursor: pointer;
+            }
+
+            &:active {
+                background: #3e9b4f;
+                color: #FFF;
+            }
+        }
+    }
   }
 `;
