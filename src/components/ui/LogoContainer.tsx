f2e18b1e1eb83daf5df90c075daab3fede72be49
Anhpati <romain-felicianne@hotmail.fr>
2024-06-11 23:19:08 +0200

    ♻️ F03 : Extract Hero logo container styles to LogoContainer.

--- src/components/ui/LogoContainer.tsx
+++ src/components/ui/LogoContainer.tsx
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+
 export const LogoContainer = () => {
   return (
-    <div className="logo-container">
+    <LogoContainerStyled>
       <div className="img-container">
         <img src="/aguacate.svg" alt="Logo de la Ruta del Bienestar" />
       </div>
@@ -8,6 +10,48 @@ export const LogoContainer = () => {
         <h1>Ruta <span>del Bienestar</span></h1>
         <sub>- en Jardín -</sub>
       </div>
-    </div>
+    </LogoContainerStyled>
   )
 }
+
+const LogoContainerStyled = styled.div`
+  width: 190px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .img-container {
+      width: 80px;
+
+      img {
+          width: 100%;
+      }
+  }
+
+  .logo-title-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      h1 {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin: 0;
+          font-size: 72px;
+          font-weight: 500;
+          line-height: 0.9;
+
+          span {
+              font-size: 24px;
+          }
+      }
+
+      sub {
+          margin-top: 5px;
+          font-family: "Noto Serif", serif;
+          font-size: 18px;
+          font-weight: 300;
+      }
+  }
+`;
