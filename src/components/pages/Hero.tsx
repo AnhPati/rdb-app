c862a17d0261702fe55fff7810f768678da15ac6
Anhpati <romain-felicianne@hotmail.fr>
2024-06-11 23:16:25 +0200

    ♻️ F02 : HERO > Extract Hero logo container to ui LogoContainer.

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,18 +1,11 @@
 import styled from "styled-components";
 import backgroundHero from "./../../assets/hero-rdb.jpg"
+import { LogoContainer } from "../ui/LogoContainer";
 
 const Hero = () => {
     return (
         <HeroStyled>
-            <div className="logo-container">
-                <div className="img-container">
-                    <img src="/aguacate.svg" alt="Logo de la Ruta del Bienestar" />
-                </div>
-                <div className="logo-title-container">
-                    <h1>Ruta <span>del Bienestar</span></h1>
-                    <sub>- en Jardín -</sub>
-                </div>
-            </div>
+            <LogoContainer />
         </HeroStyled>
     )
 }
---
+++ src/components/ui/LogoContainer.tsx
@@ -0,0 +1,13 @@
+export const LogoContainer = () => {
+  return (
+    <div className="logo-container">
+      <div className="img-container">
+        <img src="/aguacate.svg" alt="Logo de la Ruta del Bienestar" />
+      </div>
+      <div className="logo-title-container">
+        <h1>Ruta <span>del Bienestar</span></h1>
+        <sub>- en Jardín -</sub>
+      </div>
+    </div>
+  )
+}
