d096afe825769c59251622e13df8f97458c5f4b6
Anhpati <romain-felicianne@hotmail.fr>
2024-06-11 16:52:10 +0200

    🚧 F01 : HERO > Add hero content.

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,6 +1,16 @@
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div>
+            <div className="logo-container">
+                <div className="img-container">
+                    <img src="/aguacate.svg" alt="Logo de la Ruta del Bienestar" />
+                </div>
+                <h1>
+                    Ruta del Bienestar
+                    <sub>- Jardin -</sub>
+                </h1>
+            </div>
+        </div>
     )
 }
 
