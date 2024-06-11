903d34c3e715a994d70558e8dcbb0d33e1cee289
Anhpati <romain-felicianne@hotmail.fr>
2024-06-11 17:06:21 +0200

    💄 F01 : HERO > Add Hero layout styles.

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,17 +1,62 @@
+import styled from "styled-components";
+
 const Hero = () => {
     return (
-        <div>
+        <HeroStyled>
             <div className="logo-container">
                 <div className="img-container">
                     <img src="/aguacate.svg" alt="Logo de la Ruta del Bienestar" />
                 </div>
-                <h1>
-                    Ruta del Bienestar
+                <div className="logo-title-container">
+                    <h1>Ruta <span>del Bienestar</span></h1>
                     <sub>- Jardin -</sub>
-                </h1>
+                </div>
             </div>
-        </div>
+        </HeroStyled>
     )
 }
 
 export default Hero
+
+const HeroStyled = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+
+    .logo-container {
+        width: 190px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .img-container {
+            width: 80px;
+
+            img {
+                width: 100%;
+            }
+        }
+
+        .logo-title-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            h1 {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                margin: 0;
+                font-size: 72px;
+
+                span {
+                    font-size: 24px;
+                }
+            }
+
+            sub {
+                font-size: 18px;
+            }
+        }
+    }
+`;
