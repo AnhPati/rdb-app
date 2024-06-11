ac8911c584ff7f4bf3bf258a127cdf6108ab80bf
Anhpati <romain-felicianne@hotmail.fr>
2024-06-11 17:24:15 +0200

    💄 F01 : HERO > Add Hero background image styles.

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import backgroundHero from "./../../assets/hero-rdb.jpg"
 
 const Hero = () => {
     return (
@@ -22,6 +23,7 @@ const HeroStyled = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background: linear-gradient(60deg, #F5FBF5, 85%, transparent),right url(${backgroundHero});
 
     .logo-container {
         width: 190px;
