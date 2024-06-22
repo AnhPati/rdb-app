197c960dc8b394d4f35d5ff1a879c6c8eb605247
Anhpati <romain-felicianne@hotmail.fr>
2024-06-22 22:47:58 +0200

    🚧 F05 : DATAS > Create users fake datas and map users data to CardsContainer.

--- src/components/pages/Home/CardsContainer.tsx
+++ src/components/pages/Home/CardsContainer.tsx
@@ -1,6 +1,16 @@
+import { users } from './../../../data/usersData'
 const CardsContainer = () => {
     return (
-        <div>CardsContainer</div>
+        <div>
+            {users.map(user => {
+                return (
+                    <section>
+                        <h2>{user.event.eventName}</h2>
+                        <img src={user.event.eventImgs[0]} alt={user.event.eventName} />
+                    </section>
+                )
+            })}
+        </div>
     )
 }
 
