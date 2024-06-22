1d5597f1ebadf894eb1881a6b77868844d98ca18
Anhpati <romain-felicianne@hotmail.fr>
2024-06-23 00:02:30 +0200

    💄 F06 : CARD > Add CardsContainer section available places container styles.

--- src/components/pages/Home/CardsContainer.tsx
+++ src/components/pages/Home/CardsContainer.tsx
@@ -92,5 +92,22 @@ const CardsContainerStyled = styled.div`
                 }
             }
         }
+
+        .available_places-container {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            height: 50px;
+            width: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: #3E9B4F;
+            color: #FFF;
+            font-family: "Noto Serif", serif;;
+            font-size: 9px;
+            font-weight: 700;
+            border-radius: 50%;
+        }
     }
 `;
