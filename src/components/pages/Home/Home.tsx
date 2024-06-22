5cf02df9951d38315f3d837ccb6a93c0165aa32f
Anhpati <romain-felicianne@hotmail.fr>
2024-06-22 17:57:02 +0200

    ✨ F05 : CARDSCONTAINER > Create CardsContainer and add to Home.

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components"
 import Hero from "./Hero"
 import Navbar from "./Navbar/Navbar"
+import CardsContainer from "./CardsContainer"
 
 const Home = () => {
     return (
         <HomeStyled>
             <Hero />
             <Navbar />
+            <CardsContainer />
         </HomeStyled>
     )
 }
