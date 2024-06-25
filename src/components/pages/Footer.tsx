906ccad248592542c39703921348d9d57171a3c0
Anhpati <romain-felicianne@hotmail.fr>
2024-06-25 21:08:38 +0200

    🚧 F07 : FOOTER > Update Footer layout and add links content.

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -1,11 +1,18 @@
 import { BsFilePdf } from "react-icons/bs";
 import styled from "styled-components";
+import rdbProgram from "./../../assets/rdb-1-programmacion.jpeg"
+
 
 const Footer = () => {
     return (
         <FooterStyled>
-            <a href=""><BsFilePdf /> Descargar nuestro programa</a>
-            <a href="">Hecho con ❤️ por AnhPati</a>
+            <div>
+                <a href={rdbProgram} download><BsFilePdf /> Descargar nuestro programa</a>
+            </div>
+
+            <p>
+                Hecho con ❤️ por <a href="https://github.com/AnhPati">AnhPati</a>
+            </p>
             {/* <a href="">A cerca de la Ruta del Bienestar</a> */}
         </FooterStyled>
     )
@@ -16,7 +23,8 @@ export default Footer
 const FooterStyled = styled.footer`
     height: 100px;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: space-around;
     align-items: center;
     gap: 30px;
     padding: 10px;
