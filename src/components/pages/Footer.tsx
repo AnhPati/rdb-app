60becdb0ce4a8ce6df08d7bdb7a0767e739b0638
Anhpati <romain-felicianne@hotmail.fr>
2024-06-25 21:41:04 +0200

    💄 F07 : FOOTER > Add download link styles and fonts styles.

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -24,9 +24,47 @@ const FooterStyled = styled.footer`
     height: 100px;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
-    gap: 30px;
+    gap: 15px;
     padding: 10px;
     background: #3E9B4F;
+
+    div {
+        width: 100%;
+        flex: 1;
+        display: flex;
+        align-items: center;
+        
+
+        a {
+            padding: 10px;
+            border: solid 2px #2A7E3B;
+            border-radius: 5px;
+            background: #FBFEFB;
+            max-width: 360px;
+            color: #2A7E3B;
+            font-family: "Noto Serif", serif;
+            font-size: 16px;
+            font-weight: 700;
+            text-transform: uppercase;
+            display: flex;
+            text-decoration: none;
+
+            svg {
+                font-size: 25px;
+            }
+        }
+    }
+
+    p {
+        margin: 0;
+        color: #FFF;
+        font-family: "Noto Serif", serif;
+        font-size: 11px;
+
+        a {
+            color: #FFF;
+        }
+    }
 `;
