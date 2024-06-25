c40306c06d13128fee6dd5fb275d9faafe4b3642
Anhpati <romain-felicianne@hotmail.fr>
2024-06-25 19:14:54 +0200

    ✨ F07 : FOOTER > Create Footer and add to Main.

---
+++ src/components/pages/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <a href="">Descargar nuestro programa</a>
+            <a href="">Hecho con ❤️ por AnhPati</a>
+            <a href="">A cerca de la Ruta del Bienestar</a>
+        </footer>
+    )
+}
+
+export default Footer
